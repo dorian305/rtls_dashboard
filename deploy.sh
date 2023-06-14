e48779e7c11ca6921c5eb48956b3d9e96d8f9340
Dorian Manjarić <manjaricdorian0@gmail.com>
2023-06-14 21:50:05 +0200

    slava

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-# This script deploys the application to the production server.
-rsync -va \
---exclude='.git/' \
---exclude='deploy' \
---exclude='.gitignore' \
---exclude='README.md' \
-. blooddrunk@manjaric.com:rtls.manjaric.com/dashboard/
